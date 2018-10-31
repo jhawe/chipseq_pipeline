b7b7e7228a554c6ffd24c3d6b271a2210bfcbd9a
Johann Hawe <johann.hawe@helmholtz-muenchen.de>
2018-10-31 16:12:13 +0100

    Started with creation of peak universe. Added first scripts

---
+++ scripts/remove_blacklisted_peaks.R
@@ -0,0 +1,47 @@
+# -------------------------------------------------------------------------------
+#' Remove the blacklisted encode peaks from the called peaks of a sample.
+#' Removes all peaks overlapping with a blacklisted peak.
+#'
+#' @author Johann Hawe <johann.hawe@helmholtz-muenchen.de>
+# -------------------------------------------------------------------------------
+sink(file=snakemake@log[[1]], append=F, type="output")
+
+# -------------------------------------------------------------------------------
+print("Loading libraries and scripts.")
+# -------------------------------------------------------------------------------
+suppressPackageStartupMessages(library(GenomicRanges))
+suppressPackageStartupMessages(library(rtracklayer))
+
+# -------------------------------------------------------------------------------
+print("Getting snakemake params.")
+# -------------------------------------------------------------------------------
+# inputs
+fpeaks <- snakemake@input$peaks
+fblacklist <- snakemake@input$blacklist
+
+# output
+fout <- snakemake@output[[1]]
+
+# -------------------------------------------------------------------------------
+print("Processing peaks.")
+# -------------------------------------------------------------------------------
+peaks <- read.table(fpeaks, header=F, sep="\t")
+peaks <- with(peaks, GRanges(V1 ,IRanges(V2, V3)))
+blacklist <- import(fblacklist)
+
+# get indices of overlapping peaks
+idxs <- overlapsAny(peaks, blacklist)
+peaks <- peaks[idxs]
+
+print(paste0("Removed ", sum(!idxs)), " peaks from peak list."))
+
+# export the new peak list
+export(peaks, fout)
+
+# -------------------------------------------------------------------------------
+print("SessionInfo:")
+# -------------------------------------------------------------------------------
+sessionInfo()
+
+sink()
+
